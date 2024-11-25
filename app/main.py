1432a822f3e6b9ac86e83d124b45f32f427f3de0
Golnazmes <gmesbahi.gm@gmail.com>
2024-11-25 12:07:30 -0700

    update readme

--- app/main.py
+++ app/main.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import pipeline
-import numpy as np
 
-model = pipeline('sentiment-analysis')
+# Load the sentiment analysis model
+sentiment_pipeline = pipeline(
+    "sentiment-analysis",
+    model="cardiffnlp/twitter-roberta-base-sentiment-latest",
+)
 
-
+# Initialize FastAPI app
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {'message': 'Model Inference API'}
 
-@app.post('/infer')
-def infer(data: str):
-    prediction = model(data)
-    return {'prediction': prediction}
+# Define input schema
+class InputData(BaseModel):
+    input: str
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Model Inference API"}
+
+
+@app.post("/infer")
+def infer(data: InputData):
+    # Extract the text input
+    input_text = data.input
+    # Perform sentiment analysis
+    analysis = sentiment_pipeline(input_text)
+    return {"analysis": analysis}
